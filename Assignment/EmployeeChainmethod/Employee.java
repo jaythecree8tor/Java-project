c364c39ab88e6ef68368687b6fb446c07a74b338
jaythecree8tor <femibellajordan@gamil.com>
2022-11-26 18:53:12 +0100

    Assignment

---
+++ Assignment/EmployeeChainmethod/Employee.java
@@ -0,0 +1,47 @@
+package Assignment.EmployeeChainmethod;
+
+public class Employee {
+    String name="Jhon doe";
+    int id= 444;
+    String position= "Senior Dev";
+    double salary=5500.45;
+     
+    public void display()
+    {
+       System.out.println("Name: " + name + "\nId: " + id + "Position: " + position + "\nSalary: " + salary);
+    }
+    public String showString()
+    {
+        System.out.println("Name: " + name + "\nId: " + id + "Position: " + position + "\nSalary: " + salary);
+        return "Hello...Here's our employee details";
+    }
+    public double showPercent()
+    {
+        System.out.println("Name: " + name + "\nId: " + id + "Position: " + position + "\nSalary: " + salary);
+        return 99.6;
+    }
+    public int showNumber()
+    {
+        System.out.println("Name: " + name + "\nId: " + id + "Position: " + position + "\nSalary: " + salary);
+        return 021;
+    }
+    public Employee showObj1()
+    {
+        System.out.println("Name: " + name + "\nId: " + id + "Position: " + position + "\nSalary: " + salary);
+        return this;
+    }
+    public Employee showObj2()
+    {
+        System.out.println("Name: " + name + "\nId: " + id + "Position: " + position + "\nSalary: " + salary);
+        return this;
+    }
+    public Employee showObj3()
+    {
+        System.out.println("Name: " + name + "\nId: " + id + "Position: " + position + "\nSalary: " + salary);
+        return this;
+    }
+
+
+
+
+}
---
+++ Assignment/Employeepackage/employeepack.java
@@ -0,0 +1,8 @@
+package Assignment.Employeepackage;
+import Assignment.EmployeeChainmethod.Employee;
+public class employeepack {
+    public static void main(String[] args) {
+        Employee st=new Employee();
+        st.showObj1().showObj2().showObj3().display();;
+    }
+}
