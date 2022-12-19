a7968f12b818106a2ab10fdd2fea8f1f587b873b
jaythecree8tor <femibellajordan@gamil.com>
2022-12-19 05:55:20 +0100

    push

--- co/guiApp/PanelExample.java
+++ co/guiApp/PanelExample.java
@@ -1,9 +1,29 @@
 package guiApp;
-
+import java.awt.*;
+import javax.swing.*;
 public class PanelExample {
 
     PanelExample(){
 
     }
-    
+     
+    void initComponents()
+    {
+         JFrame f=new JFrame("APTECH FORM");
+         JPanel panel= new JPanel();
+         panel.setBounds(0,0,500,500);
+         panel.setBackground(Color.gray);
+         JButton b1= new JButton("Button 1");
+         b1.setBounds(5, 10, 80, 30);
+         JButton b2= new JButton("Cancel");
+         b2.setBounds(5, 20, 80, 30);
+         panel.add(b1);
+         panel.add(b2);
+         f.add(panel);
+         f.setSize(400,400);
+       
+         
+
+    }
+
 }
